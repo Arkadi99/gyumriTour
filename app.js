15faa5d9c5a4bb0be412bcc3bc0dceb759c0b669
Avo <tadevosyanavo@gmail.com>
2022-09-10 17:20:22 +0400

    test commit

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ import usersRouter from "./routes/users";
 
 const app = express();
 
-
+app.use(cors({
+  origin: '*'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
